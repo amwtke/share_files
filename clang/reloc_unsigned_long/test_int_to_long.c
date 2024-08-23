65dd40415114a43dd54515dce6d19bc02f5ea181
amwtke <amwtke@gmail.com>
2024-08-23 14:29:24 +0000

    数字转字符串

--- clang/reloc_unsigned_long/test_int_to_long.c
+++ clang/reloc_unsigned_long/test_int_to_long.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<string.h>
+void itoc(unsigned long);
 int main(){
 	int a = 0x810003bf;
 	unsigned long map = 0x0- 0xffffffff80000000;
@@ -10,7 +12,41 @@ int main(){
 	printf("b is %#lx\n",b);
 	printf("ptr is %#lx\n",ptr);
 
-	void* p = 0x82814000;
-	p = p - (void*)0xffffffff81000000;
-	printf("0x82814000 - 0xffffffff81000000 =  %#lx\n",p);
+	unsigned long  p = 0x0000000002814067;
+	p = p - (unsigned long)0xffffffff80000000;
+	printf("test =  %#lx\n",p);
+	
+	printf("a is %d\n",a);
+	itoc(0xffffffff80000000);
+}
+void itoc(unsigned long n){
+	char s[100] = {0};
+	int i = 0, j, flag = 0;
+	char temp;
+	if(n < 0)//为负数
+	{
+		n = -n;
+		flag = 1;
+	}
+	if(n == 0)//数字为0
+	{
+		s[i++] = '0';
+	}
+	while(n)
+	{
+		s[i++] = n % 10 +'0'; //取出每一位数字
+		n = n/10;
+	}
+	if(flag)
+	{
+		s[i++] = '-';
+	}
+	j = strlen(s);
+	for(i = 0; i < j/2; i++)  //顺序调换
+	{
+		temp = s[i];
+		s[i] = s[j-i-1];
+		s[j-i-1] = temp;
+	}
+	printf("number is %s\n",s);
 }
